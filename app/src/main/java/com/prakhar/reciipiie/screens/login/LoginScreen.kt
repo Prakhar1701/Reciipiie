b2ddb79efe97a4f796e7076cc0972544f0eb9aee
Prakhar1701 <prakharagrawal170101@outlook.com>
2024-05-12 16:09:51 +0530

    Add login screen background image

--- app/src/main/java/com/prakhar/reciipiie/screens/login/LoginScreen.kt
+++ app/src/main/java/com/prakhar/reciipiie/screens/login/LoginScreen.kt
@@ -1,13 +1,33 @@
 package com.prakhar.reciipiie.screens.login
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import com.prakhar.reciipiie.R
 
 @Composable
 fun LoginScreen(
     navController: NavHostController,
     viewModel: LoginScreenViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 ) {
-    Text(text = "Login Screen")
+    Scaffold { contentPadding ->
+        Surface(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.login_background),
+                contentDescription = "Login screen background image",
+                contentScale = ContentScale.FillHeight,
+            )
+        }
+    }
 }
